8a589590ed754021297df747321ad00ef67021eb
Henrique Miranda <miranda.henrique@gmail.com>
2018-10-24 10:51:49 +0200

    Modify gkkp plotter and reader to use the new GKQ file with the matrix elements.

--- abipy/eph/gkkp.py
+++ abipy/eph/gkkp.py
@@ -6,7 +6,7 @@ import os
 import re
 import numpy as np
 from collections import defaultdict
-from abipy.iotools import ETSF_Reader
+from abipy.electrons.ebands import ElectronsReader
 from monty.string import marquee
 
 __all__ = [
@@ -26,74 +26,84 @@ class GKKPPlotter():
         lines = []; app = lines.append
         return "\n".join(lines)
 
-class GKKPReader():
+class GKKPReader(ElectronsReader):
     """
     Read a single GKKP file
     """
     def __init__(self,filename):
-        self.filename = filename
+        super(GKKPReader, self).__init__(filename)
 
-        # each file corresponds to a q-point and perturbation
-        with ETSF_Reader(filename) as db:
-            # read the qpoint
-            self.qpt = db.read_value('current_q_point')
-            #read kptopt
-            self.kptopt = db.read_value('kptopt')
+        # read the qpoint
+        self.qpt = self.read_value('qpoint')
 
-            #TODO: add consistency check here
-            #read number of kpoints
-            self.nkpoints = db.read_dimvalue('number_of_kpoints')
-            self.kpoints = db.read_value('reduced_coordinates_of_kpoints')
-            self.nbands = db.read_dimvalue('max_number_of_states')
-            self.natoms = db.read_dimvalue('number_of_atoms')
-            self.nsppol = int(db.read_dimvalue('product_mband_nsppol')/self.nbands)
-           
-            #read the lattice 
-            self.structure = db.read_structure()
+        #read kptopt
+        self.kptopt = self.read_value('kptopt')
 
+        #TODO: add consistency check here
+        #read number of kpoints
+        self.nkpoints = self.read_dimvalue('number_of_kpoints')
+        self.kpoints = self.read_value('reduced_coordinates_of_kpoints')
+        self.nbands = self.read_dimvalue('max_number_of_states')
+        self.natoms = self.read_dimvalue('number_of_atoms')
+        self.nsppol = self.read_dimvalue('number_of_spins')
+        
         # read the perturbation
         basename = os.path.basename(filename)
-        self.ipert = int(re.findall('([0-9]+)',basename)[0])-1
 
-    def get_allk_gkkp(self,spin,ib1,ib2):
-        """
-        TODO: Should take slices of the netcdf file on the fly
-        without reading all the contents
-        """
-        gkkp = self.get_all_gkkp(self)
-        return gkkp[ib1,:,ib2,spin]
+    @property
+    def ebands(self):
+        """read ebands"""
+        return self.read_ebands()
 
-    def get_one_gkkp(self,ispin,ikpt,ib1,ib2):
+    @property
+    def structure(self):
+        """read the lattice"""
+        return self.read_structure()
+     
+    def get_allk_gkkp(self,ispin,imu,ib1,ib2):
         """
         TODO: Should take slices of the netcdf file on the fly
         without reading all the contents
         """
         gkkp = self.get_all_gkkp()
-        return gkkp[ib1,ikpt,ib2,ispin]
+        return gkkp[ispin,:,imu,ib1,ib2]
+
+    def get_allm_gkkp(self,ispin,ikpt,ib1,ib2):
+        """
+        TODO: Should take slices of the netcdf file on the fly
+        without reading all the contents
+        """
+        gkkp = self.get_all_gkkp()
+        return gkkp[ispin,ikpt,:,ib1,ib2]
+
+    def get_one_gkkp(self,ispin,ikpt,imu,ib1,ib2):
+        """
+        TODO: Should take slices of the netcdf file on the fly
+        without reading all the contents
+        """
+        gkkp = self.get_all_gkkp()
+        return gkkp[ispin,ikpt,imu,ib1,ib2]
 
     def get_all_gkkp(self):
         """
         Get gkkp matrix elements from the file
         """
-        with ETSF_Reader(self.filename) as db:
-            gkkp_reim = db.rootgrp.variables['second_derivative_eigenenergies_actif'][:]
-            gkkp_reim = gkkp_reim.reshape(self.nbands,self.nkpoints,self.nbands,self.nsppol,2)
-            gkkp = gkkp_reim[:,:,:,:,0] + 1j*gkkp_reim[:,:,:,:,1]
+        gkkp_reim = self.rootgrp.variables['gkq'][:]
+        gkkp = gkkp_reim[:,:,:,:,:,0] + 1j*gkkp_reim[:,:,:,:,:,1]
         return gkkp
 
     def __str__(self):
         lines = []; app = lines.append
         app(marquee(self.__class__.__name__, mark="=")) 
         app('qpoint: '+("%12.8lf "*3)%tuple(self.qpt))
-        app('ipert:  %d'%self.ipert)
         app('nbands: %d'%self.nbands)
         app('natoms: %d'%self.natoms)
-        app('nkpoints: %d'%self.natoms)
+        app('nkpoints: %d'%self.nkpoints)
         return "\n".join(lines)
 
 class MultiGKKPReader():
     """
-    Read the electron-phonon matrix elements from eph_task 5
+    Read the electron-phonon matrix elements from eph_task 2
     In the init we only read the basic informations of the different gkkp files.
     The matrix elements are only loaded uppon request.
 
@@ -101,56 +111,34 @@ class MultiGKKPReader():
     and provide a common interface to access the data.
     Currently works with GKKP files produced with eph_task 2
     """
-    def __init__(self,filenames_list,ddb_file):
+    def __init__(self,filenames_list):
 
-        self.ddb_file = ddb_file
         self.filenames_list = filenames_list
-        self._qpt_ipert_dict = defaultdict(dict)
 
+        self.qpoints = []
+        self.gkqs = []
         for filename in filenames_list:
-          
-            #read data from this GKKP file 
-            gkkp = GKKPReader(filename)
- 
-            #store qpoint and perturbation
-            qpt = tuple(gkkp.qpt)
-            ipert = gkkp.ipert
-            self._qpt_ipert_dict[qpt][ipert] = filename
+
+            #open the reader
+            gkq = GKKPReader(filename)
+            self.gkqs.append(gkq)
+
+            #store qpoint
+            qpt = gkq.qpt
+            self.qpoints.append(qpt)
+
+            if np.linalg.norm(qpt) < 1e-8:
+                self.kpoints = gkq.kpoints
+                self.nbands = gkq.nbands
+                self.natoms = gkq.natoms
+                self.nmodes = self.natoms*3
 
             #store the structure
-            self.structure = gkkp.structure
+            self.structure = gkq.structure
 
-        self.kpoints = gkkp.kpoints
-        self.qpoints = list(self._qpt_ipert_dict.keys())
-        self.nbands = gkkp.nbands
-        self.natoms = gkkp.natoms
-        self.nmodes = self.natoms*3
 
-    def _get_phonons(self):
-        """Call anaddb to diagonalize the dynamical matrices in the DDB file"""
-        from abipy import abilab
-        ddb = abilab.abiopen(self.ddb_file)
-        phonon_modes = []
-        phonon_freqs = []
-        for qpoint in self.qpoints:
-            phbands = ddb.anaget_phmodes_at_qpoint(qpoint)
-            phonon_modes.append(phbands.phdispl_cart)
-            phonon_freqs.append(phbands.phfreqs)
-        phonon_freqs = np.vstack(phonon_freqs)
-        phonon_modes = np.vstack(phonon_modes)
-        return phonon_freqs, phonon_modes
-
-    @property
-    def phonon_modes(self):
-        if not hasattr(self,'_phonon_modes'):
-            self._phonon_freqs, self._phonon_modes = self._get_phonons()
-        return self._phonon_modes
-
-    @property
-    def phonon_freqs(self):
-        if not hasattr(self,'_phonon_freqs'):
-            self._phonon_freqs, self._phonon_modes = self._get_phonons()
-        return self._phonon_freqs
+    def __getitem__(self,iqpt):
+        return self.gkqs[iqpt]
 
     @property
     def nkpoints(self):
@@ -168,28 +156,22 @@ class MultiGKKPReader():
 
     @classmethod
     def from_flow(cls,flow):
-        """Find all the GKKP files in this flow and a DDB file"""
+        """Find all the GKQ files in this flow"""
         filenames_list = []
         ddb_file = None
         for work in flow:
             for task in work:
-                filenames_list.extend(task.outdir.has_abiext("GKK",single_file=False))
-                has_ddb_file = task.indir.has_abiext("DDB")
-                if has_ddb_file: ddb_file = has_ddb_file
-        if ddb_file is None: raise FileNotFoundError('DDB file not found in flow')
-        return cls(filenames_list,ddb_file)
+                filenames_list.extend(task.outdir.has_abiext("GKQ",single_file=False))
+        return cls(filenames_list)
 
     @classmethod
     def from_work(cls,flowdir):
-        """Find all GKKP files in this work and a DDB file"""
+        """Find all GKQ files in this work"""
         filenames_list = []
         ddb_file = None
         for task in work:
-            filenames_list.extend(task.outdir.has_abiext("GKK",single_file=False))
-            has_ddb_file = task.indir.has_abiext("DDB")
-            if has_ddb_file: ddb_file = has_ddb_file
-        if ddb_file is None: raise FileNotFoundError('DDB file not found in flow')
-        return cls(filenames_list,ddb_file)
+            filenames_list.extend(task.outdir.has_abiext("GKQ",single_file=False))
+        return cls(filenames_list)
 
     def get_gkkp_fix_k(self,ispin,kpt,ib1,ib2,imu,cartesian=True):
         """
@@ -226,13 +208,7 @@ class MultiGKKPReader():
         """
         Get one matrix element at a time using the indexes of the q and k point
         """
-        gkkp_rec = []
-        for i in range(self.nmodes):
-            reader = self.get_reader(iqpt,i)
-            gkkp = reader.get_one_gkkp(ispin,ikpt,ib1,ib2)
-            gkkp_rec.append(gkkp)
-
-        gkkp_rec = np.array(gkkp_rec)
+        gkkp_rec = self[iqpt].get_allm_gkkp(ispin,ikpt,ib1,ib2)
 
         #project cartesian
         gkkp_cartesian = np.einsum('a,ma->m', gkkp_rec, self.tcartesian)
@@ -242,21 +218,6 @@ class MultiGKKPReader():
         t = self.phonon_modes[iqpt]
         gkkp_modes = np.einsum('a,ma->m', gkkp_cartesian, t)
         return gkkp_modes[imu]
-
-    def get_filename(self,iqpt,ipert):
-        """
-        Get the filename containing the information of this qpoint and perturbation
-        """
-        qpt = self.qpoints[iqpt]
-        return self._qpt_ipert_dict[qpt][ipert]
- 
-    def get_reader(self,iqpt,ipert):
-        """
-        Get reader for a particular qpoint and perturbation
-        an ETSF_Reader is returned, the user is responsible for closing
-        """   
-        filename = self.get_filename(iqpt,ipert)
-        return GKKPReader(filename)
 
     def get_iqpt(self,qpt):
         """
@@ -288,6 +249,6 @@ class MultiGKKPReader():
             app(("%12.8lf "*3)%tuple(kpt))
         app('qpoints:')
         for qpt in self.qpoints:
-            app(("%12.8lf "*3)%qpt)
+            app(("%12.8lf "*3)%tuple(qpt))
         return "\n".join(lines)
 
